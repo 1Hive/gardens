b1c99f1bd9e47d67e218ac41a6cef5f3da07f01f
fabriziovigevani <fabriziovigevani@gmail.com>
2021-04-22 17:31:27 -0300

    Rename OrganizationTemplate for GardensTemplate

--- packages/subgraph/scripts/build-manifest.sh
+++ packages/subgraph/scripts/build-manifest.sh
@@ -14,11 +14,11 @@ cat $DATA
 
 mustache \
   -p manifest/templates/sources/Organizations.yaml \
-  -p manifest/templates/sources/OrganizationTemplates.yaml \
+  -p manifest/templates/sources/GardensTemplates.yaml \
   -p manifest/templates/contracts/Agreement.template.yaml \
   -p manifest/templates/contracts/ConvictionVoting.template.yaml \
   -p manifest/templates/contracts/DisputableVoting.template.yaml \
   -p manifest/templates/contracts/Organization.template.yaml \
-  -p manifest/templates/contracts/OrganizationTemplate.template.yaml \
+  -p manifest/templates/contracts/GardensTemplate.template.yaml \
   $DATA \
   subgraph.template.yaml > subgraph.yaml

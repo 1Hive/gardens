d3a4c0c077479e9138ea592f6e5caec89cd446ba
Lint Action <lint-action@samuelmeuli.com>
2021-04-30 20:34:37 +0000

    Fix code style issues with Prettier

--- packages/hardhat/scripts/new-dao.ts
+++ packages/hardhat/scripts/new-dao.ts
@@ -156,10 +156,7 @@ export default async function main(log = console.log): Promise<any> {
     log(`Approval for gardens payment made.`)
   }
 
-  const createDaoTxOne = async (
-    gardensTemplate: GardensTemplate,
-    log: Function
-  ): Promise<string> => {
+  const createDaoTxOne = async (gardensTemplate: GardensTemplate, log: Function): Promise<string> => {
     const createDaoTxOneTx = await gardensTemplate.createDaoTxOne(
       existingToken,
       orgTokenName,
@@ -189,10 +186,7 @@ export default async function main(log = console.log): Promise<any> {
     log(`Tx create token holders completed.`)
   }
 
-  const createDaoTxTwo = async (
-    gardensTemplate: GardensTemplate,
-    log: Function
-  ): Promise<void> => {
+  const createDaoTxTwo = async (gardensTemplate: GardensTemplate, log: Function): Promise<void> => {
     const createDaoTxTwoTx = await gardensTemplate.createDaoTxTwo(
       [issuanceTargetRatio, issuanceMaxAdjustmentPerSecond],
       [decay, maxRatio, weight, minThresholdStakePercentage],
@@ -202,10 +196,7 @@ export default async function main(log = console.log): Promise<any> {
     log(`Tx two completed.`)
   }
 
-  const createDaoTxThree = async (
-    gardensTemplate: GardensTemplate,
-    log: Function
-  ): Promise<void> => {
+  const createDaoTxThree = async (gardensTemplate: GardensTemplate, log: Function): Promise<void> => {
     const createDaoTxThreeTx = await gardensTemplate.createDaoTxThree(
       daoId,
       agreementTitle,
@@ -220,8 +211,8 @@ export default async function main(log = console.log): Promise<any> {
     log(`Tx three completed.`)
   }
 
-  const gardensTemplate = await getGardensTemplate(mainAccount);
-  const createNewToken = existingToken == ZERO_ADDRESS; // As opposed bring your own token
+  const gardensTemplate = await getGardensTemplate(mainAccount)
+  const createNewToken = existingToken == ZERO_ADDRESS // As opposed bring your own token
 
   if (createNewToken) {
     await approveHoneyPayment(gardensTemplate, log)

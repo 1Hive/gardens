9aa978f791c14a0d45384671817905c27bc6e85a
fabriziovigevani <fabriziovigevani@gmail.com>
2021-05-06 17:58:28 -0300

    Models: Add missing data

--- packages/connector/src/models/Supporter.ts
+++ packages/connector/src/models/Supporter.ts
@@ -1,10 +1,11 @@
-import { CastData, IGardenConnector, StakeData, StakeHistoryData, SupporterData } from '../types'
+import { CastData, IGardenConnector, OrganizationData, StakeData, StakeHistoryData, SupporterData, UserData } from '../types'
 
 export default class Supporter implements SupporterData {
   #connector: IGardenConnector
 
   readonly id: string
-  readonly address: string
+  readonly user: UserData
+  readonly organization: OrganizationData
   readonly representative: string
   readonly casts: CastData[]
   readonly stakes: StakeData[]
@@ -14,7 +15,8 @@ export default class Supporter implements SupporterData {
     this.#connector = connector
 
     this.id = data.id
-    this.address = data.address
+    this.user = data.user
+    this.organization = data.organization
     this.representative = data.representative
     this.casts = data.casts
     this.stakes = data.stakes

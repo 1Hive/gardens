1c12f3469240a1c55c8f98a0ad14d32b247285d0
0xGabi <gabrielpk.18@gmail.com>
2021-09-06 01:51:54 -0300

    Subgraph: handle signed gardens for users

--- packages/subgraph/src/mappings/Agreement.ts
+++ packages/subgraph/src/mappings/Agreement.ts
@@ -5,8 +5,15 @@ import {
   ActionDisputed as ActionDisputedEvent,
   ActionSettled as ActionSettledEvent,
   ActionChallenged as ActionChallengedEvent,
+  Signed as SignedEvent,
 } from '../../generated/templates/Agreement/Agreement'
-import { getProposalEntity, getProposalEntityId, loadTokenData } from '../helpers/index'
+import {
+  getProposalEntity,
+  getProposalEntityId,
+  loadOrCreateOrg,
+  loadOrCreateUser,
+  loadTokenData,
+} from '../helpers/index'
 import { STATUS_DISPUTED, STATUS_DISPUTED_NUM, STATUS_SETTLED, STATUS_SETTLED_NUM } from '../statuses'
 
 /* eslint-disable @typescript-eslint/no-use-before-define */
@@ -66,6 +73,20 @@ export function handleActionChallenged(event: ActionChallengedEvent): void {
   proposal.settlementOffer = challengeData.value4
   proposal.pausedAt = event.block.timestamp
   proposal.save()
+}
+
+export function handleSigned(event: SignedEvent): void {
+  const agreementApp = AgreementContract.bind(event.address)
+  const gardenAddress = agreementApp.kernel()
+
+  const org = loadOrCreateOrg(gardenAddress)
+  const user = loadOrCreateUser(event.params.signer)
+
+  const currentGardensSigned = user.gardensSigned
+  currentGardensSigned.push(org.id)
+  user.gardensSigned = currentGardensSigned
+
+  user.save()
 }
 
 function createArbitratorFee(proposalId: string, id: string, feeToken: Address, feeAmount: BigInt): void {

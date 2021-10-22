2ac98fb4b4fa8bb7110a34aca5313c2106200d11
0xGabi <gabrielpk.18@gmail.com>
2021-10-22 18:55:50 -0300

    Connector(docs): update type and include typedoc comments

--- packages/connector/src/helpers/index.ts
+++ packages/connector/src/helpers/index.ts
@@ -1,10 +1,10 @@
 export { toMilliseconds, currentTimestampEvm } from './time'
 export { bn, formatBn, PCT_BASE, PCT_DECIMALS } from './numbers'
 
-export function buildProposalId(proposalNumber: number, appAddress: string) {
+export function buildProposalId(proposalNumber: number, appAddress: string): string {
   return `appAddress:${appAddress}-proposalId:0x${proposalNumber.toString(16)}`
 }
 
-export function buildSupporterId(supporterAddress: string, orgAddress: string) {
+export function buildSupporterId(supporterAddress: string, orgAddress: string): string {
   return `${supporterAddress}-org:${orgAddress}`
 }
